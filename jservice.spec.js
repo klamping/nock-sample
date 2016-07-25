6ddf38b449c5f22963e6bc778c829c3b4e519686
Kevin Lamping <klamping@gmail.com>
2016-07-25 16:01:05 -0500

    updated with nock to make tests pass

--- jservice.spec.js
+++ jservice.spec.js
@@ -1,9 +1,17 @@
 var assert = require("assert");
 var jservice = require("./jservice");
+var nock = require("nock");
+
+var jserviceApi = nock("http://jservice.io/api/");
 
 describe("JService", function() {
   it("should allow category requests", function (done) {
-    var expectedCategory = "classic lit";
+    var expectedCategory = "my category";
+
+    jserviceApi.get("/category?id=10044")
+      .reply(200, {
+        title: expectedCategory
+      });
 
     jservice.getCategory(10044, function(category) {
       assert.equal(category, expectedCategory);
@@ -13,6 +21,11 @@ describe("JService", function() {
 
   it("should get random questions", function (done) {
     var expectedClue = "This is the clue.";
+
+    jserviceApi.get("/random")
+      .reply(200, [{
+        question: expectedClue
+      }])
 
     jservice.getRandomQuestion(function(question) {
       assert.equal(question, expectedClue);
